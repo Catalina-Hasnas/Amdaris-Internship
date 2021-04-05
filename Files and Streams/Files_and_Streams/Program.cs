3f89030e4889593a9eef8002ecc05300763b35ba
Catalina Hasnas <catalinahasnas1996@gmail.com>
2021-04-05 15:56:44 +0300

    files and streams assignment

---
+++ Files and Streams/Files_and_Streams/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Files_and_Streams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\CutepPie\Desktop\Amdaris Internship\Files and Streams\Files_and_Streams";
+            string fileName = "Test.txt";
+
+            var filePath = Path.Combine(rootPath, fileName);
+
+            if (!File.Exists(filePath))
+            {
+                using (FileStream fs = File.Create(filePath));
+            }
+            else
+            {
+                Console.WriteLine($"File {fileName} already exists.");
+                return;
+            }
+
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+         
+            lines.Add("first line");
+            lines.Add("second line");
+            lines.Add("third line");
+
+            File.WriteAllLines(filePath, lines);
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
