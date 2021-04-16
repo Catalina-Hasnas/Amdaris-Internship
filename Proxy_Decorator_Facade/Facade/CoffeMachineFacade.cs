8784c3601f70ab66a08fedb8428fd7afb4a30027
Catalina Hasnas <catalinahasnas1996@gmail.com>
2021-04-16 14:02:03 +0300

    added Facade deign pattern

---
+++ Proxy_Decorator_Facade/Facade/CoffeMachineFacade.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy_Decorator_Facade.Facade
+{
+    public class CoffeMachineFacade
+    {
+        protected CoffeeMachine _coffeeMachine;
+
+        public CoffeMachineFacade(CoffeeMachine coffeeMachine)
+        {
+            _coffeeMachine = coffeeMachine;
+        }
+        public void MakeCoffee()
+        {
+            _coffeeMachine.GetElectricity();
+            _coffeeMachine.GrindCoffeeBeans();
+            _coffeeMachine.HeatWater();
+            _coffeeMachine.PourCoffee();
+        }
+    }
+}
---
+++ Proxy_Decorator_Facade/Facade/CoffeeMachine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy_Decorator_Facade.Facade
+{
+    public class CoffeeMachine
+    {
+        public void GetElectricity()
+        {
+            Console.WriteLine("getting electricity...");
+        }
+        public void GrindCoffeeBeans()
+        {
+            Console.WriteLine("grinding coffee beans...");
+        }
+        public void HeatWater()
+        {
+            Console.WriteLine("heating water to the right temperature...");
+        }
+        public void PourCoffee()
+        {
+            Console.WriteLine("pouring coffee...");
+        }
+    }
+}
